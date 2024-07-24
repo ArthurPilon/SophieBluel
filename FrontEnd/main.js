f05051165dbf454069bdf3c0ce28bdc368627553
ArthurPilon <arthurpilon0@gmail.com>
2024-07-24 12:02:48 +0200

    Upload fonctionnel

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -2,6 +2,7 @@ const apiURL = "http://localhost:5678/api/";
 const storedToken = localStorage.getItem("token");
 let works = null;
 let modal = null;
+let filters = null;
 
 console.log(18, document.body.classList);
 
@@ -13,14 +14,14 @@ if (storedToken) {
 
 async function loadGalleryData() {
   let data = await fetch(apiURL + "works");
-  console.log(1, data);
   works = await data.json();
   let imgGalleryDiv = document.getElementById("img-gallery");
-  console.log(2, works, imgGalleryDiv);
+  imgGalleryDiv.innerHTML = "";
 
   works.forEach((work) => {
     const figureElem = document.createElement("figure");
     figureElem.classList.add("category" + work.categoryId);
+    figureElem.dataset.id = work.id;
 
     const imgElem = document.createElement("img");
     imgElem.src = work.imageUrl;
@@ -39,7 +40,7 @@ async function loadGalleryData() {
 async function loadFilters() {
   let data = await fetch(apiURL + "categories");
   console.log(3, data);
-  let filters = await data.json();
+  filters = await data.json();
   let filtersDiv = document.querySelector(".filterslist");
   console.log(4, filters, filtersDiv);
 
@@ -60,6 +61,7 @@ async function loadFilters() {
 window.onload = async () => {
   await loadFilters();
   await loadGalleryData();
+  await CreateModalFilters();
 
   let filtreObjets = document.querySelectorAll(".filterslist button");
   let imgGalleryDiv = document.getElementById("img-gallery");
@@ -67,9 +69,7 @@ window.onload = async () => {
   let buttonTous = document.getElementById("butontous");
 
   filtreObjets.forEach((btn) => {
-    console.log(7, btn);
     btn.addEventListener("click", () => {
-      console.log(8);
       filtreObjets.forEach((btn) => {
         btn.classList.remove("active");
       });
@@ -116,7 +116,7 @@ const openModal = function (e) {
     .querySelector(".js-modal-stop")
     .addEventListener("click", stopPropagation);
 
-  loadModalGallery(works);
+  loadModalGallery();
 };
 
 const closeModal = function (e) {
@@ -132,40 +132,221 @@ const closeModal = function (e) {
     .removeEventListener("click", stopPropagation);
 
   modal = null;
+
+  location.reload();
 };
 
 const stopPropagation = function (e) {
   e.stopPropagation();
 };
 
-async function loadModalGallery(items) {
+async function loadModalGallery() {
   const modalGalleryDiv = document.querySelector(".modal-gallery");
   modalGalleryDiv.innerHTML = "";
 
-  items.forEach((item) => {
+  works.forEach((item) => {
     const figureElem = document.createElement("figure");
-    const imgElem = document.createElement("img");
-    const trashIcon = document.createElement("i");
+    figureElem.dataset.id = item.id;
 
+    const imgElem = document.createElement("img");
     imgElem.src = item.imageUrl;
     imgElem.alt = item.title;
 
-    trashIcon.classList.add("fa-solid", "fa-trash-can", "trashicon");
-    trashIcon.style.color = "#ffffff";
-    trashIcon.style.position = "absolute";
-    trashIcon.style.top = "5px"; // Adjust as needed
-    trashIcon.style.right = "5px"; // Adjust as needed
-    trashIcon.style.cursor = "pointer";
-    // trashIcon.style.background = "#000";
+    const trashButton = document.createElement("button");
+    const trashIcon = document.createElement("i");
 
-    figureElem.style.position = "relative"; // Ensure the icon is positioned relative to the figure
+    trashIcon.classList.add("fa-solid", "fa-trash-can");
+    trashButton.classList.add("trashicon");
+
+    addDeleteWorkListener(trashButton, item.id, figureElem);
+
+    figureElem.style.position = "relative";
     figureElem.appendChild(imgElem);
-    figureElem.appendChild(trashIcon);
+    figureElem.appendChild(trashButton);
+    trashButton.appendChild(trashIcon);
 
     modalGalleryDiv.appendChild(figureElem);
   });
 }
 
+function addDeleteWorkListener(button, id, figureElem) {
+  button.addEventListener("click", function () {
+    deleteWork(id);
+  });
+}
+const deleteWork = async function (id, figureElem) {
+  const url = apiURL + "works/" + id;
+  const response = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      Authorization: "Bearer " + storedToken,
+    },
+  });
+  if (response.ok) {
+    await loadGalleryData();
+    loadModalGallery();
+  } else {
+    console.error(
+      "Erreur lors de la suppression de l'élément :",
+      response.statusText
+    );
+  }
+};
+
+document
+  .getElementById("buttontomodal2")
+  .addEventListener("click", function () {
+    document.querySelectorAll(".modal1").forEach(function (element) {
+      element.style.display = "none";
+    });
+
+    document.querySelectorAll(".modal2").forEach(function (element) {
+      element.style.display = "block";
+    });
+  });
+
 document.querySelectorAll(".js-modal").forEach((a) => {
   a.addEventListener("click", openModal);
 });
+
+//faire que la flèche revienne à la page précédente.
+
+document
+  .getElementById("modal-returnbutton")
+  .addEventListener("click", function () {
+    document.querySelectorAll(".modal2").forEach(function (element) {
+      element.style.display = "none";
+    });
+
+    document.querySelectorAll(".modal1").forEach(function (element) {
+      element.style.display = "flex";
+    });
+  });
+
+// ajouter photo
+document
+  .getElementById("modal-fileinput")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    const preview = document.getElementById("modal-container-add-pic");
+    preview.innerHTML = "";
+
+    if (file) {
+      const validImageTypes = ["image/jpeg", "image/png"];
+      if (!validImageTypes.includes(file.type)) {
+        alert("Erreur : le fichier doit être une image (jpg, png)");
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const img = document.createElement("img");
+        img.src = e.target.result;
+        preview.appendChild(img);
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+
+const fileInput = document.getElementById("modal-fileinput");
+const titleInput = document.getElementById("photoTitle");
+const sendButton = document.getElementById("modal-sendbutton2");
+let categorySelect = undefined;
+
+//création du select
+async function CreateModalFilters() {
+  let modalSelect = document.getElementById("modal-select");
+  modalSelect.innerHTML = "";
+
+  // <select>
+  const selectElem = document.createElement("select");
+  selectElem.name = "Catégorie";
+  selectElem.id = "modal-form-category";
+
+  categorySelect = selectElem;
+  categorySelect.addEventListener("change", checkFormValidity);
+
+  // <option> vide
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.textContent = "";
+  selectElem.appendChild(defaultOption);
+
+  // <option> pour chaque filtre
+  filters.forEach((item) => {
+    const createOption = document.createElement("option");
+    createOption.value = item.id;
+    createOption.textContent = item.name;
+
+    selectElem.appendChild(createOption);
+  });
+  modalSelect.appendChild(selectElem);
+}
+
+// vérifier si tous les champs sont remplis
+function checkFormValidity() {
+  const file = fileInput.files[0];
+  const title = titleInput.value.trim();
+  const category = categorySelect.value;
+
+  console.log("File:", file);
+  console.log("Title:", title);
+  console.log("Category:", category);
+
+  if (
+    file !== undefined &&
+    title !== undefined &&
+    title !== "" &&
+    category !== undefined &&
+    category !== ""
+  ) {
+    sendButton.disabled = false;
+    sendButton.classList.remove("modal-sendbutton");
+    sendButton.classList.add("modal-form-category-go");
+    console.log("Button class:", sendButton.className);
+  } else {
+    sendButton.disabled = true;
+    sendButton.classList.remove("modal-form-category-go");
+    console.log("Button class:", sendButton.className);
+  }
+}
+
+fileInput.addEventListener("change", checkFormValidity);
+titleInput.addEventListener("input", checkFormValidity);
+
+sendButton.addEventListener("click", function (event) {
+  console.log("Bouton envoyer cliqué");
+
+  const file = fileInput.files[0];
+  const title = titleInput.value.trim();
+  const category = categorySelect.value;
+
+  console.log("Fichier:", file);
+  console.log("Titre:", title);
+  console.log("Catégorie:", category);
+
+  const uploadData = new FormData();
+  uploadData.append("image", file);
+  uploadData.append("title", title);
+  uploadData.append("category", category);
+
+  // Envoyer les données à l'API
+  fetch(apiURL + "works", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + storedToken,
+    },
+    body: uploadData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Réponse de l'API:", data);
+    })
+    .catch((error) => {
+      console.error("Erreur lors de l'envoi des données à l'API:", error);
+    });
+});
+// Essayer de réccupérer les infos du form, les convertir en JSON, les transmettre
+
+//On ne devrait pas avoir besoin de recharger la page pour voir que le projet a été supprimé. Assurez-vous donc de répondre à la question suivante :
+//Comment retirer des éléments du DOM après avoir reçu la confirmation de la suppression de l’entrée en base de données ?
